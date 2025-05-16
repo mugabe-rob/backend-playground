2a6904ddfc5a015cc026c877d25feb0d31a20611
mugabe-rob <mugaberobert222004644@gmail.com>
2025-05-16 10:48:34 +0200

    Add API for courses Management

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,22 @@
-import express from 'express';
+const express = require('express');
+const app = express();
 
-// const bookRouter = require('../config/modules');
+app.use(express.json());
 
-import booksRouter from './books/index.js';
+const food= [
 
-const router = express. Router();
+    {id:1, name:"Matoke", price:200},
+    {id:2, name:"Piza", price:200},
+    {id:3, name:"Rice", price:200},
+    {id:4, name:"Sweets", price:200},
+    {id:5, name:"Juice", price:200},
 
-router.use('/', booksRouter);
+];
 
-export default router;	
+app.get('/food', (req, res) => {
+    res.send([food]);
+});
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port,() => console.log('We are listening on port ${port}'));

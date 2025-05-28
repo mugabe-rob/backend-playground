14dcf1a3062d848c54fd216b361eb817a42cc368
mugabe-rob <mugaberobert222004644@gmail.com>
2025-05-28 23:44:18 +0200

    Merge all feature branches and organize project structure

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -12,14 +12,17 @@ const getProducts = async (req, res) => {
 }
 
 const getProduct = async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
     }
-    catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const createProduct = async (req, res) => {
     try {
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,22 +1,9 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+// const bookRouter = require('../config/modules');
+import booksRouter from './books/index.js';
 
-app.use(express.json());
+const router = express.Router();
 
-const food= [
+router.use('/', booksRouter);
 
-    {id:1, name:"Matoke", price:200},
-    {id:2, name:"Piza", price:200},
-    {id:3, name:"Rice", price:200},
-    {id:4, name:"Sweets", price:200},
-    {id:5, name:"Juice", price:200},
-
-];
-
-app.get('/food', (req, res) => {
-    res.send([food]);
-});
-
-//PORT
-const port = process.env.PORT || 3000;
-app.listen(port,() => console.log('We are listening on port ${port}'));
+export default router;
